235fbd10a427eac45b6bcb6d9603101f04ee7ad4
Phutiane <82708335+PhutianeMichael@users.noreply.github.com>
2025-11-11 15:09:41 +0200

    Refactor cart item component and improve error handling

--- src/app/cart/cart-item/cart-item.component.ts
+++ src/app/cart/cart-item/cart-item.component.ts
@@ -11,14 +11,9 @@ import { CartItem } from '../cart';
   templateUrl: './cart-item.component.html',
 })
 export class CartItemComponent {
-
   @Input({required: true}) cartItem!: CartItem;
-
-  // Quantity available (hard-coded to 8)
-  // Mapped to an array from 1-8
   qtyArr = [...Array(8).keys()].map(x => x + 1);
 
-  // Calculate the extended price
   exPrice = this.cartItem?.quantity * this.cartItem?.product.price;
 
   onQuantitySelected(quantity: number): void {
--- src/app/utilities/http-error.service.ts
+++ src/app/utilities/http-error.service.ts
@@ -11,15 +11,10 @@ export class HttpErrorService {
   }
 
   private httpErrorFormatter(err: HttpErrorResponse): string {
-    // In a real world app, we may send the error to some remote logging infrastructure
-    // instead of just logging it to the console
-    // console.error(err);
     let errorMessage = '';
     if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       errorMessage = `An error occurred: ${err.error.message}`;
     } else {
-      // The backend returned an unsuccessful response code.
       errorMessage = `Server returned code: ${err.status}, error message is: ${err.statusText}`;
     }
     return errorMessage;
